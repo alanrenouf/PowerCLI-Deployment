4052665aa35410017d962084286c2a2960de51e2
Alan Renouf <ajw.renouf@gmail.com>
2015-02-03 19:34:04 -0800

    Updated vShield 5.5 deployment

--- vShield 5.5.x/Deployment.ps1
+++ vShield 5.5.x/Deployment.ps1
@@ -1,33 +1,71 @@
-﻿# Load OVF/OVA configuration into a variable  
-$ovffile = "C:\Software\VMware-vShield-Manager-5.5.3-2081508.ova"  
-$ovfconfig = Get-OvfConfiguration $ovffile  
-   
-# Get the values to use for deployment  
-$VMHost = Get-Cluster "Cluster Site A" | Get-VMHost | Sort MemoryGB | Select -first 1  
-$Datastore = $VMHost | Get-datastore | Sort FreeSpaceGB -Descending | Select -first 1  
-$Network = Get-VirtualPortGroup -Name "VM Network" -VMHost $vmhost    
-$VMName = "VSM01"
-$vsmip = "192.168.110.150"
-$vsmgateway = "192.168.110.1"
-$vsmnetmask = "255.255.255.0"
-$vsmhostname = $VMName
- 
-# Fill out the OVF/OVA configuration parameters   
-$ovfconfig.NetworkMapping.VSMgmt.value = $Network   
-$ovfconfig.Common.vsm_cli_passwd_0.value = "VMware1!"  
-$ovfconfig.Common.vsm_cli_en_passwd_0.value = "VMware1!" 
+﻿$vShieldInstallFile = "C:\Temp\VMware-vShield-Manager-5.5.3-2175697.ova"
+$vShieldName = "VCNS01"
+$vShieldNetwork = "VM Network"
+$vShieldIP = "192.168.1.210"
+$vShieldSNM = "255.255.255.0"
+$vShieldDGW = "192.168.1.1"
+$vShieldDNS = "192.168.1.1"
+$vShieldCluster = "Home-Cluster"
+$vShieldPassword = "VMware1!"
+$vShieldvC = "192.168.1.200"
+$vShieldvCUser = "Administrator@vsphere.local"
+$vShieldvCPass = "VMware1!"
+	
+if (!(get-pssnapin -name VMware.VimAutomation.Core -erroraction 'SilentlyContinue')) {
+	Write-Host "[INFO] Adding PowerCLI Snapin"
+	add-pssnapin VMware.VimAutomation.Core -ErrorAction 'SilentlyContinue'
+	if (!(get-pssnapin -name VMware.VimAutomation.Core -erroraction 'SilentlyContinue')) {
+		Write-Host "[ERROR] PowerCLI Not installed, please install from Http://VMware.com/go/PowerCLI"
+	} Else {
+		Write-Host "[INFO] PowerCLI Snapin added"
+	}
+    Connect-VIServer $vShieldvC -User $vShieldvCUser -Password $vShieldvCPass
+}
+	
+$vShieldSpaceNeededGB = "5"
+Write-Host "[INFO] Selecting host for $vShieldName from $vShieldCluster Cluster"
+$vShieldVMHost =  Get-Cluster $vShieldCluster | Get-VMHost | Where {$_.PowerState -eq "PoweredOn" -and $_.ConnectionState -eq "Connected" } | Get-Random
+Write-Host "[INFO] $vShieldVMHost selected for $vShieldName"
+Write-Host "[INFO] Selecting Datastore for $vShieldName"
+$vShieldDatastore = $vShieldVMHost | Get-Datastore | Where {$_.ExtensionData.Summary.MultipleHostAccess} | Where {$_.FreeSpaceGB -ge $vShieldSpaceNeededGB} | Get-Random
+if (!$vshieldDatastore) { Write-Host "[ERROR] No Available Shared datastores with $vShieldSpaceNeededGB GB available" ; Exit }
+Write-Host "[INFO] $vShieldDatastore selected for $vShieldName"
 
-# Deploy the OVF/OVA with the config parameters  
-$VSMVM = Import-VApp -Source $ovffile -OvfConfiguration $ovfconfig -Name $VMName -VMHost $vmhost -Datastore $datastore -DiskStorageFormat thin  
+$Settings = Get-OvfConfiguration $vShieldInstallFile
+$Settings.NetworkMapping.VSMgmt.value = $vShieldNetwork
+$Settings.Common.vsm_cli_en_passwd_0.value = $vShieldPassword
+$Settings.Common.vsm_cli_passwd_0.value = $vShieldPassword
 
-# Set the IP Address
-$key = "machine.id"
-$value = "ip_0={0}&gateway_0={1}&computerName={2}&netmask_0={3}&markerid=1&reconfigToken=1" -f $vsmip, $vsmgateway, $vsmhostname, $vsmnetmask 
+Write-Host "[INFO] Importing $vShieldName from $vShieldInstallFile"
+$vShieldDeployedVMTask = $vShieldVMHost | Import-vApp -OvfConfiguration $Settings -Name $vShieldName -Source $vShieldInstallFile -Datastore $vShieldDatastore -Force -RunAsync
+do {
+	Sleep 1
+    Write-progress -Activity "Deploying $vShieldName" -PercentComplete $($vShieldDeployedVMTask.PercentComplete) -Status "$($vShieldDeployedVMTask.PercentComplete)% completed"
+		
+} until ($vShieldDeployedVMTask.PercentComplete -eq 100 )
+Write-Host "[INFO] $vShieldName deployed and the task result was $($vShieldDeployedVMTask.State)"
 
-$vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec 
-$vmConfigSpec.extraconfig += New-Object VMware.Vim.optionvalue 
-$vmConfigSpec.extraconfig[0].Key=$key 
-$vmConfigSpec.extraconfig[0].Value=$value 
-$ApplyIP = $vsmvm.ExtensionData.ReconfigVM_Task($vmConfigSpec) 
+If ($vShieldDeployedVMTask.State -ne "success") {
+		Write-Host "[ERROR] Unable to deploy vShield, deploy failed with $($vShieldDeployedVMTask.TerminatingError)"
+        Exit
+} Else {
+    $vShieldDeployedVM = Get-VM $vShieldName
+    	
+	$NetworkChange = $vShieldDeployedVM | Get-NetworkAdapter 
+    If ($NetworkChange.NetworkName -ne $vShieldNetwork) {
+        Write-Host "[INFO] Reconfiguring Network on $vShieldName to join $vShieldNetwork"
+        $NetworkChange | Set-NetworkAdapter -NetworkName $vShieldNetwork -Confirm:$false
+    }
+	$key = "machine.id" 
+	$value = "ip_0={0}&gateway_0={1}&computerName={2}&netmask_0={3}&markerid=1&reconfigToken=1" -f $vShieldIP, $vShieldDGW, $vShieldName, $vShieldSNM
+	$vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec 
+	$vmConfigSpec.extraconfig += New-Object VMware.Vim.optionvalue 
+	$vmConfigSpec.extraconfig[0].Key=$key 
+	$vmConfigSpec.extraconfig[0].Value=$value 
 
- 
+	Write-Host "[INFO] Reconfiguring $vShieldName after deployment"
+	$SetConfig = $vShieldDeployedVM.ExtensionData.ReconfigVM_Task($vmConfigSpec) 	
+    Write-Host "[INFO] Power On $vShieldName for first time"
+	$vShieldDeployedVM | Start-VM | Out-Null
+	Write-Host "[INFO] $vShieldName deployment and configuration completed."
+}
